efbed9e8e23f40ca5ff8bb1b92e90bc891233967
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2020-12-10 11:26:55 -0800

    Add files via upload

---
+++ Sale Price Calculator/Sale Price Calculator/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.calcSalePrice = New System.Windows.Forms.Button()
+        Me.exitBtn = New System.Windows.Forms.Button()
+        Me.lblSalePrice = New System.Windows.Forms.Label()
+        Me.txtDiscountPercent = New System.Windows.Forms.TextBox()
+        Me.txtRetailPrice = New System.Windows.Forms.TextBox()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'calcSalePrice
+        '
+        Me.calcSalePrice.Location = New System.Drawing.Point(4, 294)
+        Me.calcSalePrice.Name = "calcSalePrice"
+        Me.calcSalePrice.Size = New System.Drawing.Size(132, 34)
+        Me.calcSalePrice.TabIndex = 0
+        Me.calcSalePrice.Text = "Calculating Sales Price"
+        Me.calcSalePrice.UseVisualStyleBackColor = True
+        '
+        'exitBtn
+        '
+        Me.exitBtn.Location = New System.Drawing.Point(271, 294)
+        Me.exitBtn.Name = "exitBtn"
+        Me.exitBtn.Size = New System.Drawing.Size(52, 35)
+        Me.exitBtn.TabIndex = 1
+        Me.exitBtn.Text = "Exit"
+        Me.exitBtn.UseVisualStyleBackColor = True
+        '
+        'lblSalePrice
+        '
+        Me.lblSalePrice.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSalePrice.Location = New System.Drawing.Point(121, 142)
+        Me.lblSalePrice.Name = "lblSalePrice"
+        Me.lblSalePrice.Size = New System.Drawing.Size(173, 23)
+        Me.lblSalePrice.TabIndex = 2
+        '
+        'txtDiscountPercent
+        '
+        Me.txtDiscountPercent.Location = New System.Drawing.Point(121, 91)
+        Me.txtDiscountPercent.Name = "txtDiscountPercent"
+        Me.txtDiscountPercent.Size = New System.Drawing.Size(173, 20)
+        Me.txtDiscountPercent.TabIndex = 3
+        '
+        'txtRetailPrice
+        '
+        Me.txtRetailPrice.Location = New System.Drawing.Point(121, 42)
+        Me.txtRetailPrice.Name = "txtRetailPrice"
+        Me.txtRetailPrice.Size = New System.Drawing.Size(173, 20)
+        Me.txtRetailPrice.TabIndex = 4
+        '
+        'lblMessage
+        '
+        Me.lblMessage.BackColor = System.Drawing.SystemColors.Control
+        Me.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMessage.Location = New System.Drawing.Point(12, 249)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(311, 23)
+        Me.lblMessage.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(51, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Retail Price:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(5, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Discount Percentage:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(57, 152)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Sale Price:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblSalePrice)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtRetailPrice)
+        Me.GroupBox1.Controls.Add(Me.txtDiscountPercent)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 21)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(311, 205)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(354, 340)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.exitBtn)
+        Me.Controls.Add(Me.calcSalePrice)
+        Me.Name = "Form1"
+        Me.Text = "Sale Price Calculator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents calcSalePrice As Button
+    Friend WithEvents exitBtn As Button
+    Friend WithEvents lblSalePrice As Label
+    Friend WithEvents txtDiscountPercent As TextBox
+    Friend WithEvents txtRetailPrice As TextBox
+    Friend WithEvents lblMessage As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Sale Price Calculator/Sale Price Calculator/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+
+    Private decRetail As Decimal     'Retail price
+    Private decPercentage As Decimal 'Discount Percentage
+
+    Private Function ValidateInputFields() As Boolean
+        'Converting each of the input fields. Returns False if any field
+        'is invalid, and display a suitable error message
+
+        If Not Decimal.TryParse(txtRetailPrice.Text, decRetail) Then
+            lblMessage.Text = "Retail price must be numeric"
+            Return False
+        End If
+
+        If Not Decimal.TryParse(txtDiscountPercent.Text, decPercentage) Then
+            lblMessage.Text = "Discount percentage must be numeric"
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Function CalculateSalePrice(ByVal decRetail As Decimal,
+                                ByVal decPercentage As Decimal) As Decimal
+        'Calculating and returning Sale Price
+        Dim decSalePrice As Decimal
+        decSalePrice = decRetail - (decRetail * decPercentage)
+        Return decSalePrice
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtSalePriceClick(sender As Object, e As EventArgs) Handles lblSalePrice.Click
+
+    End Sub
+
+    Private Sub calcSalePrice_Click(sender As Object, e As EventArgs) Handles calcSalePrice.Click
+        Dim decSalePrice As Decimal
+        lblMessage.Text = String.Empty
+
+        'If the input is valid, display the sale price.
+        If ValidateInputFields() Then
+            decSalePrice = CalculateSalePrice(decRetail, decPercentage)
+            lblSalePrice.Text = decSalePrice.ToString("c")
+        End If
+    End Sub
+
+    Private Sub exitBtn_Click(sender As Object, e As EventArgs) Handles exitBtn.Click
+        Me.Close()
+    End Sub
+End Class
